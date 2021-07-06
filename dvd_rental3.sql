5d525010bb8e1615af7234bdb2cd135014fa8062
Cagatay ONDER <79524893+cagatayonder@users.noreply.github.com>
2021-07-06 15:43:29 +0300

    Create dvd_rental3.sql

---
+++ dvd_rental3.sql
@@ -0,0 +1,4 @@
+1)SELECT*FROM country WHERE country ILIKE 'a%a';
+2)SELECT *FROM country WHERE country ILIKE '%_____n';
+3)SELECT title FROM film WHERE title ILIKE '%t%t%t%t%';
+4)SELECT * From film Where title LIKE 'C%' AND length > 90 AND rental_rate = 2.99 ;
