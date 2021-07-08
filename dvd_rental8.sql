3e63d512d946deb647f7d4575f2e043b1708a92e
Cagatay ONDER <79524893+cagatayonder@users.noreply.github.com>
2021-07-08 23:34:30 +0300

    Create dvd_rental8.sql

---
+++ dvd_rental8.sql
@@ -0,0 +1,11 @@
+ 1) CREATE TABLE employee(
+    id INTEGER,
+    name VARCHAR(50),
+    birthday DATE,
+    email VARCHAR(100)
+  );
+  2) DONE .
+  3)   UPDATE employee
+      SET name = 'Çağatay',birthday = '20/06/1994'
+      WHERE id =1;
+  4) DELETE FROM employee WHERE id= 1;
